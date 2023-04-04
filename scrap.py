3582c01477132a07da6f54009cf69d1ea2fb54e5
ignacio <ignacio.reparaz@fluxit.com.ar>
2023-04-04 09:50:27 -0300

    changes v1.0

--- scrap.py
+++ scrap.py
@@ -1,58 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import logging
+import aiohttp
+import asyncio
 
-class Scrapping():
-    
+logging.basicConfig(level=logging.INFO)
+# TODO:
+# dejar una función main que invoque al Scraper al ejecutar el script por fuera de la clase
+
+
+class Scrapper:
     def __init__(self):
-      self.url = 'https://simpsonizados.me/serie/los-simpson/'
-      self.__db = 'simpson_db.json'
+        self.url = "https://simpsonizados.me/serie/los-simpson/"
+        self.__db = "episode_filename.json"
 
-    def get_data(self):
-      req = requests.get(self.url)
-      page = req.text
-      soup = BeautifulSoup(page, 'lxml')
-      all_episodes = soup.find_all('ul', class_='episodios')
-      result = []
-      temp = 1
-      for i in range(0, len(all_episodes)):
-          print('Realizando temporada: ', temp)
-          ep_count = 1
-          for j in range(1, len(all_episodes[i]) + 1):
-              class_name = 'mark-' + str(j)
-              episode = all_episodes[i].find('li', class_ = class_name)
-              if not episode:
-                continue
-              dicc = {
-                'Nro capitulo': episode.find('div', class_ = 'numerando').get_text().split('-')[1].strip(),
-                'Nombre capitulo': episode.find('a').get_text(),
-                'Temporada': episode.find('div', class_ = 'numerando').get_text().split('-')[0].strip(),
-                'Fecha emision': episode.find('span', class_ = 'date').get_text(),
-                'url': episode.find('a')['href']
-              }
-              print(dicc)
-              get_resumen = requests.get(dicc['url'])
-              resumen_txt = get_resumen.text
-              resumen_soup = BeautifulSoup(resumen_txt, 'lxml')
-              description = resumen_soup.find('p').get_text()
-              dicc['Resumen'] = description
-              result.append(dicc)
-              print('Episodio nro ', ep_count, ' realizado')
-              ep_count += 1
-          temp +=1
-        
-      return result
-    
-    def write(self):
-      data = self.get_data()
-      with open(self.__db, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    async def get_description(self, description_url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(description_url) as response:
+                response_json = await response.text()
+                resumen_soup = BeautifulSoup(response_json, "lxml")
+                description = resumen_soup.find("p").get_text()
+                return description
 
-    def read(self, offset = 0, limit = None):
-      data = open(self.__db, 'r').read()
-      data_json = json.loads(data)
-      for i in range(offset, limit or len(data_json)):
-        print('\n', data_json[i], '\n')
+    async def serialize_episode(self, extracted_episodes):
+        result = []
+        for i in range(0, len(extracted_episodes)):
+            logging.info("\n Realizando temporada: ", i + 1, "\n")
+            episode = extracted_episodes[i].find_all("li")
+            for j in range(0, len(episode)):
+                dicc = {
+                    "Nro capitulo": episode[j]
+                    .find("div", class_="numerando")
+                    .get_text()
+                    .split("-")[1]
+                    .strip(),
+                    "Nombre capitulo": episode[j].find("a").get_text(),
+                    "Temporada": episode[j]
+                    .find("div", class_="numerando")
+                    .get_text()
+                    .split("-")[0]
+                    .strip(),
+                    "Fecha emision": episode[j].find("span", class_="date").get_text(),
+                    "url": episode[j].find("a")["href"],
+                }
+                logging.info(dicc)
+                dicc["Descripcion"] = await self.get_description(dicc["url"])
+                result.append(dicc)
+
+        return result
+
+    async def scrap(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url) as response:
+                response_json = await response.text()
+                soup = BeautifulSoup(response_json, "lxml")
+                extracted_episodes = soup.find_all("ul", class_="episodios")
+                seasons = await self.serialize_episode(extracted_episodes)
+                self.write(seasons)
+
+    def write(self, episodes):
+        with open(self.__db, "w", encoding="utf-8") as f:
+            json.dump(episodes, f, ensure_ascii=False, indent=4)
+
+    def episodes(self, offset=0, limit=None):
+        with open(self.__db, "r") as file:
+            read_content = json.loads(file.read())
+            logging.info(read_content[offset:limit])
 
 
 # Número del capítulo
@@ -62,7 +76,14 @@ class Scrapping():
 # Fecha de emisión
 # URL del video
 
-# c = Scrapping()
-# c.get_data()
+c = Scrapper()
+# c.scrap()
+# c.episodes(offset=2, limit=3)
+# c.scrap()
 # c.write()
 # c.read(limit=3)
+# async def main():
+#     await Scrapper().scrap()
+# main()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(c.scrap())
