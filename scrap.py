611e8460998715035dd43aed655832d52d213e95
Andrés Milla <andressmilla@gmail.com>
2023-04-11 15:35:34 -0300

    Added some refactor methods

--- scrap.py
+++ scrap.py
@@ -9,44 +9,61 @@ class Scrapping():
       self.__db = 'simpson_db.json'
 
     def get_data(self):
-      req = requests.get(self.url)
-      page = req.text
-      soup = BeautifulSoup(page, 'lxml')
-      all_episodes = soup.find_all('ul', class_='episodios')
-      result = []
-      temp = 1
-      for i in range(0, len(all_episodes)):
-          print('Realizando temporada: ', temp)
-          ep_count = 1
-          for j in range(1, len(all_episodes[i]) + 1):
-              class_name = 'mark-' + str(j)
-              episode = all_episodes[i].find('li', class_ = class_name)
-              if not episode:
-                continue
-              dicc = {
-                'Nro capitulo': episode.find('div', class_ = 'numerando').get_text().split('-')[1].strip(),
-                'Nombre capitulo': ' '.join(episode.find('a').get_text().split()),
-                'Temporada': episode.find('div', class_ = 'numerando').get_text().split('-')[0].strip(),
-                'Fecha emision': episode.find('span', class_ = 'date').get_text(),
-                'url': episode.find('a')['href']
+      extracted_episodes_catalog = []
+
+      for season_number, season in enumerate(self._extract_seasons(), start=1):        
+          for episode_number, episode in enumerate(self._extract_episodes(season), start=1):
+              print(f'Realizando temporada: {season_number}x{episode_number}...')
+
+              extracted_episode = {
+                'Nro capitulo': self._extract_episode_number(episode),
+                'Nombre capitulo': self._extract_episode_name(episode),
+                'Temporada': self._extract_season_number(episode),
+                'Fecha emision': self._extract_emission_date(episode),
+                'url': self.extract_episode_url(episode),
               }
-              print(dicc)
-              try:
-                get_resumen = requests.get(dicc['url'])
-                resumen_txt = get_resumen.text
-                resumen_soup = BeautifulSoup(resumen_txt, 'lxml')
-                description = resumen_soup.find('p').get_text()
-                dicc['Resumen'] = description
-                result.append(dicc)
+
+              try:                
+                extracted_episode['Resumen'] = self._extract_episode_summary(extracted_episode)
+                extracted_episodes_catalog.append(extracted_episode)
               except Exception as e:
-                print('Error al obtener el resumen del capítulo')
+                print('Error al obtener el resumen del capítulo', e)
               else:
-                print('Episodio nro ', ep_count, ' realizado')
-              finally:
-                ep_count += 1
-          temp +=1
+                print('Episodio nro ', extracted_episode['Nro capitulo'], ' realizado')
         
-      return result
+      return extracted_episodes_catalog
+    
+    def _extract_seasons(self):
+      soup = self._get_soup(self.url)
+      return soup.find_all('ul', class_='episodios')
+
+    def _extract_episode_summary(self, episode):
+        soup = self._get_soup(episode['url'])
+        return soup.find('p').get_text()
+
+    def extract_episode_url(self, episode):
+        return episode.find('a')['href']
+
+    def _extract_emission_date(self, episode):
+        return episode.find('span', class_ = 'date').get_text()
+
+    def _extract_season_number(self, episode):
+        return episode.find('div', class_ = 'numerando').get_text().split('-')[0].strip()
+
+    def _extract_episode_name(self, episode):
+        return ' '.join(episode.find('a').get_text().split())
+
+    def _extract_episode_number(self, episode):
+        return episode.find('div', class_ = 'numerando').get_text().split('-')[1].strip()
+
+    def _extract_episodes(self, season):
+        return season.find_all('li')
+
+    def _get_soup(self, url):
+        req = requests.get(url)
+        page = req.text
+        soup = BeautifulSoup(page, 'lxml')
+        return soup
     
     def write(self):
       data = self.get_data()
