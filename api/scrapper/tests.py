fbcfc108877a92e3ef7cee4b0e6b5de36bb487f7
ignacio <ignacio.reparaz@fluxit.com.ar>
2023-04-17 10:38:01 -0300

    replace urls with reverse

--- api/scrapper/tests.py
+++ api/scrapper/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from scrapper.models import Episode
-from rest_framework.test import APIClient
 from rest_framework import status
+from rest_framework.test import APIClient, APIRequestFactory
+from rest_framework.reverse import reverse
 from datetime import datetime
-from django.urls import reverse
 
 # Create your tests here.
 
@@ -26,22 +26,36 @@ class EpisodeTestCase(TestCase):
         "summary": "Después de varios destrozos y debido a su mala conducta, El Pequeño Ayudante de Santa Claus será vendido si no aprueba en la escuela canina.",
     }
 
+    def get_url(self, random: bool = False, params=None):
+        url = reverse("episode-list", request=self.request)
+        if random:
+            url = reverse("episode-random", request=self.request)
+
+        if params is not None:
+            url += f"?{params}"
+
+        return url
+
     def setUp(self):
+        self.request = APIRequestFactory().get("/")
         self.client = APIClient()
         Episode.objects.create(**self.episode1)
         Episode.objects.create(**self.episode2)
 
     def test_get_episode(self):
-        episode = self.client.get("/episodes/")
+        url = self.get_url()
+        episode = self.client.get(url)
         self.assertEqual(episode.status_code, status.HTTP_200_OK)
 
     def test_get_episode_per_season(self):
-        episode = self.client.get("/episodes/?season=1")
+        url = self.get_url(params="season=1")
+        episode = self.client.get(url)
         self.assertEqual(episode.status_code, status.HTTP_200_OK)
         self.assertEqual(episode.data[0]["season_number"], 1)
 
     def test_get_episode_per_season_when_not_passing_query(self):
-        episode = self.client.get("/episodes/?season")
+        url = self.get_url(params="season")
+        episode = self.client.get(url)
         self.assertEqual(episode.status_code, status.HTTP_200_OK)
         self.assertEqual(episode.data["count"], 2)
 
@@ -54,7 +68,8 @@ class EpisodeTestCase(TestCase):
             "url": "https://simpsonizados.me/capitulo/los-simpson-1x13/",
             "summary": "Marge se siente poco apreciada por parte de Homer, lo cual la motiva a hacer una llamada a una emisora de radio terapeuta, la cual Homer escucha con desilución. Homer, buscando llegar al corazón de Marge, decide darle una cena en un restaurante de lujo y contrata a una niñera para cuidar de Bart y Lisa. Le envían a la Sra. Botz, la cual Bart y Lisa pronto descubrirán que es en realidad una ladrona apodada “la niñera Bandit”. Ellos son capturados por la Sra. Botz, pero con el tiempo son liberados por Maggie. Bart y Lisa capturan a la Sra. Botz y llaman a la policía. Mientras tanto, Marge y Homer vuelven a casa y encuentran a la Sra. Botz atada. Homer desconocen su verdadera identidad, la libera y la Sra Botz hace un intento de huida justo antes de que la policía le frustre. Estrella invitada: Penny Marshall.",
         }
-        response = self.client.post("/episodes/", data)
+        url = self.get_url()
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Episode.objects.count(), 3)
         self.assertEqual(response.data["name"], "La babysitter ataca de nuevo")
@@ -68,18 +83,21 @@ class EpisodeTestCase(TestCase):
             "url": "https://simpsonizados.me/capitulo/los-simpson-1x1000/",
             "summary": "Episodio Actualizado Test",
         }
-        response = self.client.post("/episodes/", data)
+        url = self.get_url()
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Episode.objects.count(), 2)
         self.assertEqual(response.data["name"], self.episode1["name"])
         self.assertNotEqual(response.data["number"], self.episode1["number"])
 
     def test_random_episode(self):
-        episode = self.client.get("/episodes/random/")
+        url = self.get_url(random=True)
+        episode = self.client.get(url)
         self.assertIsInstance(episode.data, dict)
 
     def test_random_episode_passing_query_season_param(self):
-        episode = self.client.get("/episodes/random/?season=2")
+        url = self.get_url(random=True, params="season=2")
+        episode = self.client.get(url)
         self.assertIsInstance(episode.data, dict)
         self.assertEqual(episode.data["season_number"], 2)
 
@@ -90,14 +108,17 @@ class EpisodeTestCase(TestCase):
             "season_number": 1,
             "url": "https://simpsonizados.me/capitulo/los-simpson-1x13/",
         }
-        response = self.client.post("/episodes/", data, format="json")
+        url = self.get_url()
+        response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Episode.objects.count(), 2)
 
     def test_get_episode_per_season_when_season_do_not_exist(self):
-        episode = self.client.get("/episodes/?season=90")
+        url = self.get_url(params="season=90")
+        episode = self.client.get(url)
         self.assertEqual(episode.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_random_episode_when_season_do_not_exist(self):
-        episode = self.client.get("/episodes/random/?season=90")
+        url = self.get_url(random=True, params="season=90")
+        episode = self.client.get(url)
         self.assertEqual(episode.status_code, status.HTTP_400_BAD_REQUEST)
