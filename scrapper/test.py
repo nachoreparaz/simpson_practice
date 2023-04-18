5fc6772e85ae76558ee40a545706482e2101ce47
ignacio <ignacio.reparaz@fluxit.com.ar>
2023-04-18 12:48:21 -0300

    scrap test update post successfully and error

--- scrapper/test.py
+++ scrapper/test.py
@@ -1,8 +1,10 @@
 import unittest
+from django.test import Client
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 from datetime import datetime
 import requests
+import json
 
 from scrap import Scrapping
 
@@ -19,6 +21,7 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         self.scrapper = Scrapping()
+        self.client = Client()
 
     def test_scrap_init(self):
         self.assertEqual(
@@ -26,64 +29,22 @@ class Test(unittest.TestCase):
         )
         self.assertEqual(self.scrapper._Scrapping__db, "simpson_db.json")
 
-    @patch("scrap.requests.get")
-    def test_scrap_get_data__successfully(self, mocked_request_get):
-        # -- Arrange --
-        mocked_request_get.side_effect = [
-            MagicMock(text=read_mocked_file("page.html")),
-            MagicMock(text=read_mocked_file("episode-1x1.html")),
-            MagicMock(text=read_mocked_file("episode-1x2.html")),
-            MagicMock(text=read_mocked_file("episode-2x1.html")),
-        ]
-
-        # -- Act --
-        scrapped_episodes = self.scrapper.get_data()
-
-        # -- Assert episode detail --
-        self.assertNotEqual(len(scrapped_episodes), 0)
-
-        first_episode_of_first_season = [
-            episode for episode in scrapped_episodes if episode["number"] == 1
-        ][0]
-        date_str = "Dec. 17, 1989"
-        date_datetime = datetime.strptime(date_str, "%b. %d, %Y")
-        date_formated_str = date_datetime.strftime("%Y-%d-%m")
-        self.assertEqual(first_episode_of_first_season["name"], "Sin blanca Navidad")
-        self.assertEqual(first_episode_of_first_season["season_number"], 1)
-        self.assertEqual(
-            first_episode_of_first_season["release_date"], date_formated_str
-        )
-        self.assertEqual(
-            first_episode_of_first_season["url"],
-            "https://simpsonizados.me/capitulo/los-simpson-1x1/",
-        )
-        self.assertEqual(
-            first_episode_of_first_season["summary"], self.episode_1x1_summary
-        )
-
-        # -- Assert episode list --
-        self.assertEqual(
-            {(1, 1), (1, 2), (2, 1)},
-            {
-                (episode["season_number"], episode["number"])
-                for episode in scrapped_episodes
-            },
-        )
-
-        # -- Assert service calls --
-        self.assertEqual(mocked_request_get.call_count, 4)
-        mocked_request_get.assert_any_call(
-            "https://simpsonizados.me/serie/los-simpson/"
-        )
-        mocked_request_get.assert_any_call(
-            "https://simpsonizados.me/capitulo/los-simpson-1x1/"
-        )
-        mocked_request_get.assert_any_call(
-            "https://simpsonizados.me/capitulo/los-simpson-1x2/"
-        )
-        mocked_request_get.assert_any_call(
-            "https://simpsonizados.me/capitulo/los-simpson-2x1/"
-        )
+    @patch("scrap.requests.post")
+    def test_scrap_get_data__successfully(self, mock_post):
+        extracted_episode = {
+            "number": 1,
+            "name": "Sin blanca Navidad",
+            "season_number": 1,
+            "release_date": "1978-15-05",
+            "url": "https://simpsonizados.me/capitulo/los-simpson-1x1/",
+            "summary": "",
+        }
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_post.return_value = mock_response
+        for _ in range(3):
+            self.scrapper.api_request(extracted_episode)
+        assert mock_post.call_count == 3
 
     @patch("scrap.requests.get")
     def test_scrap_get_data__error_when_fetch_the_episodes_list(
@@ -119,26 +80,7 @@ class Test(unittest.TestCase):
             },
         )
 
-    def test_api_request_success_when_app_running(self):
-        episode_mock = {
-            "release_date": "1991-22-08",
-            "number": 16,
-            "name": "El suspenso del perro de Bart",
-            "season_number": 2,
-            "url": "https://simpsonizados.me/capitulo/los-simpson-2x16/",
-            "summary": "Después de varios destrozos y debido a su mala conducta, El Pequeño Ayudante de Santa Claus será vendido si no aprueba en la escuela canina.",
-        }
-        try:
-            response = requests.get("http://localhost:8000/")
-            self.assertEqual(response.status_code, 200)
-            post_response = Scrapping.api_request(self, episode_mock)
-            self.assertEqual(post_response.status_code, 200)
-        except requests.exceptions.RequestException:
-            self.fail("The app is not running")
-
-    def test_api_request_error_when_app_not_running(self):
-        try:
-            response = requests.get("http://localhost:8000/")
-            self.assertEqual(response.status_code, 200)
-        except requests.exceptions.RequestException:
-            self.fail("The app is not running")
+    @patch("scrap.requests.post")
+    def test_api_request_error_when_not_passing_aruments(self, mock_post):
+        with self.assertRaises(TypeError):
+            self.scrapper.api_request()
