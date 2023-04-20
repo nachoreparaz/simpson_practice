10ea609bdb2917b3a8f3edd228739ffbfcb7f0da
ignacio <ignacio.reparaz@fluxit.com.ar>
2023-04-20 11:37:57 -0300

    test auth methods

--- api/scrapper/tests.py
+++ api/scrapper/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from scrapper.models import Episode
+from django.contrib.auth.models import User
 from scrapper.views import EpisodeViewSet
-from scrapper.serializer import EpisodeSerializer
 from rest_framework import status
 from rest_framework.test import APIClient, APIRequestFactory
 from rest_framework.reverse import reverse
@@ -40,9 +40,13 @@ class EpisodeTestCase(TestCase):
     def setUp(self):
         self.request = APIRequestFactory().get("/")
         self.client = APIClient()
+        self.user = User.objects.create(username="test", password="test123")
         self.episode_viewset = EpisodeViewSet()
         Episode.objects.create(**self.episode1)
         Episode.objects.create(**self.episode2)
+
+    def tearDown(self):
+        self.user.delete()
 
     def test_get_episode(self):
         url = self.get_url()
@@ -75,7 +79,7 @@ class EpisodeTestCase(TestCase):
         self.assertEqual(episode.status_code, status.HTTP_200_OK)
         self.assertEqual(episode.data["count"], 2)
 
-    def test_create_episode(self):
+    def test_create_episode_when_user_is_authenticated(self):
         data = {
             "release_date": "1990-13-05",
             "number": 13,
@@ -84,6 +88,7 @@ class EpisodeTestCase(TestCase):
             "url": "https://simpsonizados.me/capitulo/los-simpson-1x13/",
             "summary": "Marge se siente poco apreciada por parte de Homer, lo cual la motiva a hacer una llamada a una emisora de radio terapeuta, la cual Homer escucha con desilución. Homer, buscando llegar al corazón de Marge, decide darle una cena en un restaurante de lujo y contrata a una niñera para cuidar de Bart y Lisa. Le envían a la Sra. Botz, la cual Bart y Lisa pronto descubrirán que es en realidad una ladrona apodada “la niñera Bandit”. Ellos son capturados por la Sra. Botz, pero con el tiempo son liberados por Maggie. Bart y Lisa capturan a la Sra. Botz y llaman a la policía. Mientras tanto, Marge y Homer vuelven a casa y encuentran a la Sra. Botz atada. Homer desconocen su verdadera identidad, la libera y la Sra Botz hace un intento de huida justo antes de que la policía le frustre. Estrella invitada: Penny Marshall.",
         }
+        self.client.force_authenticate(user=self.user)
         url = self.get_url()
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -99,6 +104,7 @@ class EpisodeTestCase(TestCase):
             "url": "https://simpsonizados.me/capitulo/los-simpson-1x1000/",
             "summary": "Episodio Actualizado Test",
         }
+        self.client.force_authenticate(user=self.user)
         url = self.get_url()
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -130,6 +136,7 @@ class EpisodeTestCase(TestCase):
             "season_number": 1,
             "url": "https://simpsonizados.me/capitulo/los-simpson-1x13/",
         }
+        self.client.force_authenticate(user=self.user)
         url = self.get_url()
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -146,3 +153,17 @@ class EpisodeTestCase(TestCase):
         url = self.get_url(random=True, params="season=90")
         episode = self.client.get(url)
         self.assertEqual(episode.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_episode_when_user_not_authenticated(self):
+        data = {
+            "release_date": "1990-13-05",
+            "number": 13,
+            "name": "La babysitter ataca de nuevo",
+            "season_number": 1,
+            "url": "https://simpsonizados.me/capitulo/los-simpson-1x13/",
+            "summary": "Marge se siente poco apreciada por parte de Homer, lo cual la motiva a hacer una llamada a una emisora de radio terapeuta, la cual Homer escucha con desilución. Homer, buscando llegar al corazón de Marge, decide darle una cena en un restaurante de lujo y contrata a una niñera para cuidar de Bart y Lisa. Le envían a la Sra. Botz, la cual Bart y Lisa pronto descubrirán que es en realidad una ladrona apodada “la niñera Bandit”. Ellos son capturados por la Sra. Botz, pero con el tiempo son liberados por Maggie. Bart y Lisa capturan a la Sra. Botz y llaman a la policía. Mientras tanto, Marge y Homer vuelven a casa y encuentran a la Sra. Botz atada. Homer desconocen su verdadera identidad, la libera y la Sra Botz hace un intento de huida justo antes de que la policía le frustre. Estrella invitada: Penny Marshall.",
+        }
+        url = self.get_url()
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Episode.objects.count(), 2)
